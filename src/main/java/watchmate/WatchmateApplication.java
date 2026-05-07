cf6c954ede5978f67f70f741e41a87d838e6742b
Nikola <nikolajkan8@gmail.com>
2026-05-07 17:50:01 +0200

    Initial Spring Boot setup

---
+++ src/main/java/watchmate/WatchmateApplication.java
@@ -0,0 +1,13 @@
+package watchmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/watchmate/WatchmateApplicationTests.java
@@ -0,0 +1,13 @@
+package watchmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
