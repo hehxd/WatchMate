65d5e515bfb5cc6d79e53df9b115a963d4708587
Nikola <nikolajkan8@gmail.com>
2026-05-09 22:30:38 +0200

    replace dummy token with real JWT, add input validation

--- src/main/java/watchmate/dto/LoginRequest.java
+++ src/main/java/watchmate/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package watchmate.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +10,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class LoginRequest {
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
     private String password;
 }
